fc20d7579acb46c8a7e3eb1c50d91b3b47aa2d61
unknown <shrutipandey99997@gmail.com>
2022-10-16 20:17:42 +0530

    first commit

---
+++ CompanyECommerce/Model/CommonProperty.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyECommerce.Model
+{
+    public class CommonProperty
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ CompanyECommerce/Model/DBResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace CompanyECommerce.Model
+{
+    public class DBResponse : CommonProperty
+    {
+        public DataSet ds { get; set; }
+    }
+}
